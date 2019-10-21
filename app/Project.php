cce478014c58f04c35e0257d058652ffc535e4dc
allenzhao <807590643@qq.com>
2019-10-21 22:50:41 +0800

    edit projects

--- app/Project.php
+++ app/Project.php
@@ -10,6 +10,6 @@ class Project extends Model
 
     public function user(){
 //关联
-        return $this->belongsTo('APP\User','user_ids');
+        return $this->belongsTo('APP\User','user_id');
     }
 }
